98cbe0637094ab1e53ad1f8bb5c8b361f77d605c
JuanDa512 <minhoyiyonh@gmail.com>
2023-04-12 19:47:35 -0400

    Aplicando rutas con llaves foraneas

--- Controllers/activos.controller.js
+++ Controllers/activos.controller.js
@@ -3,7 +3,7 @@ import { pool } from "../DataAccess/db.js"
 export const getActivos = async (req, res) => {
     try {
         const [result] = await pool.query(
-            "SELECT * FROM activos ORDER BY fecha_compra ASC"
+            "SELECT activos.id, activos.nombre_activo, activos.descripcion, activos.valor_compra, activos.fecha_compra, responsable.nombre, responsable.apellido, responsable.cargo, tipo_activo.tipo, area.name_area FROM activos, responsable, area, tipo_activo WHERE activos.id_responsable=responsable.id AND activos.id_area=area.id AND activos.id_tipo_activo=tipo_activo.id"
         );
         res.json(result)
     } catch (error) {
@@ -28,19 +28,17 @@ export const getActivo = async (req, res) => {
 
 export const createActivo = async (req, res) => {
     try {
-        const { nombre, descripcion, tipo, valor_compra, responsable } = req.body;
+        const { nombre_activo, descripcion, valor_compra } = req.body;
         console.log(req.body);
         const [result] = await pool.query(
-            "INSERT INTO activos(nombre, descripcion, tipo, valor_compra, responsable) VALUES (?, ?, ?, ?, ?)",
-            [nombre, descripcion, tipo, valor_compra, responsable]
+            "INSERT INTO activos(nombre_activo, descripcion, valor_compra) VALUES (?, ?, ?)",
+            [nombre_activo, descripcion, valor_compra]
         );
         res.json({
             id: result.insertId,
-            nombre, 
+            nombre_activo, 
             descripcion,
-            tipo,
             valor_compra,
-            responsable,
         });
     } catch (error) {
         return res.status(500).json({ message: error.message });
--- DataAccess/tables.sql
+++ DataAccess/tables.sql
@@ -1,17 +1,43 @@
 CREATE TABLE activos (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    id_area INTEGER NOT NULL DEFAULT 1,
+    id_tipo_activo INTEGER NOT NULL DEFAULT 1,
+    id_responsable INTEGER NOT NULL DEFAULT 1,
     nombre VARCHAR(200) NOT NULL, 
     descripcion VARCHAR(400),
     tipo VARCHAR(200),
     valor_compra FLOAT,
     fecha_compra TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     estado BOOLEAN NOT NULL DEFAULT 0,
-    responsable VARCHAR(200),
+    FOREIGN KEY (id_area) REFERENCES area(id),
+    FOREIGN KEY (id_tipo_activo) REFERENCES tipo_activo(id),
+    FOREIGN KEY (id_responsable) REFERENCES responsable(id)
+);
+
+CREATE TABLE area (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    name_area VARCHAR(50) NOT NULL,
+    bloque VARCHAR(50)NOT NULL,
+    piso VARCHAR(50)NOT NULL
+);
+
+CREATE TABLE tipo_activo (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL,
+    anosdevida INTEGER NOT NULL,
+    depreciacion FLOAT NOT NULL
+);
+
+CREATE TABLE responsable (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL,
+    apellido VARCHAR(50)NOT NULL,
+    cargo VARCHAR(50)NOT NULL
 );
 
 CREATE TABLE usuarios (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(50) NOT NULL,
-    password VARCHAR(50)NOT NULL
-    fechacreacion TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    password VARCHAR(50)NOT NULL,
+    fechacreacion TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
